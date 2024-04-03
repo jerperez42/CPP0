7af59096292d0972418682e8915791c2032885b3
jerperez42 <jerperez@student.42.fr>
2024-04-03 14:32:01 +0200

    Fixed ctrl+d

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jerperez <jerperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 14:35:21 by jerperez          #+#    #+#             */
-/*   Updated: 2024/04/03 13:12:54 by jerperez         ###   ########.fr       */
+/*   Updated: 2024/04/03 14:30:53 by jerperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,55 @@
 #include <stdio.h>
 #include <cstdlib>
 
-# define PRPT_CMD "Enter one of the three commands:"
-# define PRPT_CER "The program only accepts ADD, SEARCH and EXIT\nYou entered:"
+# define PRPT "phonebook> "
+# define MSG_CMD "Enter one of these three commands: "
+# define SEP '|'
+# define PRPT_CER "phonebook: expected ADD, SEARCH or EXIT: received "
 # define CMD_CADD "ADD"
 # define CMD_SRCH "SEARCH"
 # define CMD_EXIT "EXIT"
 
-static void	_clear_screen(void)
+static void	_ui_clear(void)
 {
 	std::cout << "\033[2J" << "\033[H";
 }
 
+static void	_ui_prompt(void)
+{
+	std::cout << MSG_CMD;
+	std::cout << CMD_CADD << SEP << CMD_SRCH << SEP << CMD_EXIT;
+	std::cout << std::endl;
+	std::cout << PRPT;
+}
+
+static void	_ui_write_error(std::string cmd)
+{
+	std::cout << PRPT_CER << '`' << cmd << "'";
+}
+
 static int	_ui_loop(PhoneBook book)
 {
-	char		buff[CIN_BUFF] = {0};
 	std::string	cmd_cadd(CMD_CADD);
 	std::string	cmd_srch(CMD_SRCH);
 	std::string	cmd_exit(CMD_EXIT);
-	std::string	cmd;
+	std::string	cmd("");
 
-	_clear_screen();
-	while (1)
+	_ui_clear();
+	while (false == std::cin.eof())
 	{
-		std::cout << PRPT_CMD;
-		std::cin.clear();
-		std::cin.getline(buff, CIN_BUFF);
-		std::cout << std::endl;
-		cmd = buff;
-		if (0 == cmd.compare(cmd_cadd))
+		_ui_prompt();
+		std::getline(std::cin, cmd);
+		if (true == std::cin.eof())
+			return (EXIT_SUCCESS);
+		else if (0 == cmd.compare(cmd_cadd))
 			book.add();
 		else if (0 == cmd.compare(cmd_srch))
 			book.search();
 		else if (0 == cmd.compare(cmd_exit))
-			return (0);
+			return (EXIT_SUCCESS);
 		else
-			std::cout << PRPT_CER << cmd << std::endl;
-		std::cout << std::endl;
+			_ui_write_error(cmd);
+		std::cout << std::endl << std::endl;
 	}
 	return (EXIT_SUCCESS);
 }
